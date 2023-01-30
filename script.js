bd3c427a2c326784160ee5ab96bac2dbb560c219
Leah Somerville <leah.somerville@stu.barren.kyschools.us>
2023-01-30 10:48:14 -0600

    Reviewed Exercise

--- script.js
+++ script.js
@@ -26,25 +26,32 @@ if (randomNum === 2 || randomNum === 4){
 
 // BONUS
 // 2
-function ifEqualToFour(isMember) {
-    return (isMember ? 'Greater than or equal to 4' : 'Less than 4');
+function ifEqualToFour(randomNum) {
+    return (randomNum ? 'Greater than or equal to 4' : 'Less than 4');
 }
 console.log(ifEqualToFour(randomNum >= 4));
 
 // 3
+// Switch Statements takes a parameter/argument and returns and runs a line are block of code based on the case (or match) that parameter/argument goes with
 switch(randomNum) {
     case 1:
-      console.log("One");
-      break;
+        console.log("One");
+        // Break key words are needed between each case so that the code for the case that follows does NOT also run
+        break;
     case 2:
-      console.log("Two");
-      break;
+        console.log("Two");
+        break;
     case 3:
-      console.log("Three");
-    break;
+        console.log("Three");
+        break;
     case 4:
         console.log("Four");
-    break;
-    default:
-      console.log("Five");
+        break;
+    case 5:
+        console.log("Five");
+        break;
+    // Default works like ELSE in a conditional statement (If the value does not match any of the cases, then it will run what is the DEFAULT)
+    // default:
+    //     console.log("This will run if nothing else matched");
+    // There is no need for a BREAK at the end of a switch statement since there are no other cases
   }
